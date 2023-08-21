a86dee3894119f627ac54713bcb288a727566275
galendex <vinicius.galende@gmail.com>
2023-08-21 20:35:17 -0300

    atualizações do primeiro  projetinho do bootcamp Santander.

--- ContaBanco.java
+++ ContaBanco.java
@@ -7,27 +7,26 @@ public class ContaBanco {
 
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
         Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
 
-        System.out.println("Por favor digite o número da sua conta: ");
+        System.out.println("Por favor, digite o número da sua conta: ");
         int conta = sc.nextInt();
 
-        System.out.println("Por favor digite o número da sua agência: ");
+        System.out.println("Por favor, digite o número da sua agência: ");
         String agencia = sc.next();
 
-        System.out.println("Por favor digite seu nome: ");
-        String nomeCliente = sc.next();
+        System.out.println("Por favor, digite seu nome: ");
+        String cliente = sc.next();
 
-        System.out.println("Seu saldo é de: ");
-        String saldoStr = sc.next();
-        double saldo = Double.parseDouble(saldoStr);
+        System.out.println("Por favor, digite seu saldo atual: ");
+        double saldo = sc.nextDouble();
 
-        System.out.println("Olá senhor " +
-                nomeCliente + ", obrigado por criar uma conta em nosso banco. Sua agência é " +
-                agencia + ", conta " +
-                conta + " e seu saldo " +
-                saldo + " já está disponível para saque!");
+        System.out.println("Olá senhor "
+                + cliente + "! Obrigado por criar uma conta em nosso banco. Sua agência é "
+                + agencia + " e conta "
+                + conta + ", seu saldo atual é de "
+                + saldo + " e já está disponível para saque!");
 
         sc.close();
     }
