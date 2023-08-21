cdce9740e8d3dc3778b44acd6590dfb023e19d51
galendex <vinicius.galende@gmail.com>
2023-08-21 19:24:47 -0300

    projetinho basico do bootcamp

---
+++ ContaBanco.java
@@ -0,0 +1,34 @@
+package udemy;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaBanco {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        System.out.println("Por favor digite o número da sua conta: ");
+        int conta = sc.nextInt();
+
+        System.out.println("Por favor digite o número da sua agência: ");
+        String agencia = sc.next();
+
+        System.out.println("Por favor digite seu nome: ");
+        String nomeCliente = sc.next();
+
+        System.out.println("Seu saldo é de: ");
+        String saldoStr = sc.next();
+        double saldo = Double.parseDouble(saldoStr);
+
+        System.out.println("Olá senhor " +
+                nomeCliente + ", obrigado por criar uma conta em nosso banco. Sua agência é " +
+                agencia + ", conta " +
+                conta + " e seu saldo " +
+                saldo + " já está disponível para saque!");
+
+        sc.close();
+    }
+}
